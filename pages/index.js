c5be0ae79c425acfc57927a0f0e55f41187a34e9
soft84ya <soft84ya@gmail.com>
2023-09-25 20:15:22 +0530

    this is html css convert to nextjs

---
+++ pages/index.js
@@ -0,0 +1,32 @@
+
+//1. Import Area
+import Head from 'next/head'
+
+
+
+
+//. Function defination area
+ function Home() {
+  return (
+    <>
+      <Head>
+        <title>welcome to my first next js application</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+        <header>head</header>
+      <main >
+       <aside>aside</aside>
+       <section>section</section>
+      </main>
+
+      <footer >
+        footer
+      </footer>
+    </>
+  )
+}
+
+
+//3.Export Area
+
+export default Home;
